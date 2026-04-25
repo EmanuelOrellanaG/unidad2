1315ad4c2084ba546388b243a9f6b5c11f4e3b5e
EmOreG <emanuel.orellana1997@gmail.com>
2026-04-25 16:08:39 -0400

    Unidad 2 Avanzado

--- src/main/java/com/example/Unidad2Otra/controller/PacienteController.java
+++ src/main/java/com/example/Unidad2Otra/controller/PacienteController.java
@@ -3,10 +3,10 @@ package com.example.Unidad2Otra.controller;
 import com.example.Unidad2Otra.model.Paciente;
 import com.example.Unidad2Otra.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,54 @@ public class PacienteController {
 
         }
         return ResponseEntity.ok(pacientes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente){
+        Paciente pac = pacienteService.save(paciente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pac);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> buscar(@PathVariable Integer id){
+        try{
+            Paciente paciente = pacienteService.findById(id);
+            return ResponseEntity.ok(paciente);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> actualizar(@PathVariable Integer id, @RequestBody Paciente paciente){
+        try{
+            Paciente pac = pacienteService.findById(id);
+            pac.setId(id);
+            pac.setRun(paciente.getRun());
+            pac.setNombre(paciente.getNombre());
+            pac.setApellido(paciente.getApellido());
+            pac.setFechaNacimiento(paciente.getFechaNacimiento());
+            pac.setCorreo(paciente.getCorreo());
+
+            pacienteService.save(pac);
+            return ResponseEntity.ok(pac);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar (@PathVariable Long id){
+        try{
+            pacienteService.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch (Exception e ){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "FUNCIONA";
     }
 }
